ac600a2ae3b6546aaff2d34ef7713112edb542da
Housam Hegazy <geohousam@gmail.com>
2023-04-14 13:56:51 +0000

    Update Slider.jsx and index.css

--- src/comp/Slider.jsx
+++ src/comp/Slider.jsx
@@ -2,24 +2,54 @@ import { useState } from "react";
 import Carousel from "react-bootstrap/Carousel";
 
 function SliderCarousel() {
-  const [counter, setCounter] = useState(0);
-  const slides = ["ahmed", "housam", "ali"];
+  let [counter, setCounter] = useState(0);
+  const slides = [
+    { imageUrl: "./images/11.jpg", p: "housam ali openion" },
+    { imageUrl: "./images/22.jpg", p: "housam ali openion" },
+    { imageUrl: "./images/33.jpg", p: "housam ali openion" },
+  ];
   let Number = 0;
   const arr = [];
   for (let i = 0; i < slides.length; i++) {
     arr.push(Number++);
   }
-  console.log(arr);
+
   return (
-    <div className="container">
-      <div style={{ width: "100%", height: "200px", background: "red" }}>
-        {slides[counter]}
+    <div style={{ background: " #a82c48" }}>
+      <div className="sliders" style={{ position: "relative", margin: "auto" }}>
+        {slides.map((item, index) => {
+          return (
+            <div
+              className="slide-container"
+              key={item.imageUrl}
+              style={{
+                left: `clac(${index * 100})%`
+              }}
+            >
+              <img
+                src={item.imageUrl}
+                width="50px"
+                height={"50px"}
+                style={{ borderRadius: "50%" }}
+                alt={"housam"}
+              />
+              <p className="p-1">{item.p}</p>
+            </div>
+          );
+        })}
       </div>
+
       <div style={{ height: "100px" }}>
-        {arr.map((item) => {
-          return <span onClick={()=>{
-            setCounter(item)
-          }} data-count={item} className="carousel"></span>;
+        {arr.map((item, index) => {
+          return (
+            <span
+              onClick={() => {
+                setCounter(counter++);
+              }}
+              data-count={item}
+              className="carousel"
+            ></span>
+          );
         })}
       </div>
     </div>

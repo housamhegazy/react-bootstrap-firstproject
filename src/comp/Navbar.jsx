460e531618bb53a467b130eead5c44463adb195a
Housam Hegazy <geohousam@gmail.com>
2023-04-14 20:50:22 +0000

    Update Navbar.jsx and index.css

--- src/comp/Navbar.jsx
+++ src/comp/Navbar.jsx
@@ -29,12 +29,13 @@ export default function Navbarr() {
             size="sm"
           />
         </div>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle
+          aria-controls="basic-navbar-nav"
+          className="text-light bg-light"
+        />
         <Navbar.Collapse id="basic-navbar-nav" style={{ flexGrow: 0 }}>
           <Nav className="me-auto ">
-            <Nav.Link href="/">
-              Home
-            </Nav.Link>
+            <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="/about">About</Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">sections</NavDropdown.Item>
